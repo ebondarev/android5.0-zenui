24c5cd602b418ee0e2a9f2e0085a4e2e6d472c7a
ebondarev <e.bondarev@inventos.ru>
2021-03-11 17:24:39 +0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css';
 import { LockScreen } from './components/lock_screen/lock_screen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <LockScreen />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <LockScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
